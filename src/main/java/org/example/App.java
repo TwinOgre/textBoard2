68343ee519468fdec9795f3d9bb00acff50bdd9a
TwinOgre <glad2you@naver.com>
2023-12-04 14:41:15 +0900

    phase2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class App {
     Scanner sc;
-    App(Scanner sc){
+
+    App(Scanner sc) {
         this.sc = sc;
     }
-    void run(){
+
+    void run() {
         System.out.println("실행됨");
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
-            if(command.equals("종료")){
+            String command = sc.nextLine().trim();
+            if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")){
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String author = sc.nextLine().trim();
             }
         }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-
         new App(sc).run();
 
         sc.close();
